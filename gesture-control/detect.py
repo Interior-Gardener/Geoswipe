0cd0fad7765013d16c8e488c6e01d1468f341bd5
RITA JOSHI <shreeyasati@gmail.com>
2025-08-15 17:24:19 +0530

    Updated

--- gesture-control/detect.py
+++ gesture-control/detect.py
@@ -3,9 +3,19 @@ import cv2
 import mediapipe as mp
 import socketio
 import time
+import numpy as np
 
 # Connect to Node.js Socket.IO server
 sio = socketio.Client()
+
+connected = False
+
+@sio.event
+def connect():
+    global connected
+    connected = True
+    print("Connected to server.")
+
 sio.connect('http://localhost:3000')
 
 # MediaPipe setup
@@ -17,26 +27,61 @@ mp_draw = mp.solutions.drawing_utils
 def distance(a, b):
     return ((a.x - b.x)**2 + (a.y - b.y)**2) ** 0.5
 
+def finger_is_open(landmarks, tip, pip):
+    return landmarks[tip].y < landmarks[pip].y
+
 def classify_gesture(landmarks):
+    wrist = landmarks[0]
     thumb_tip = landmarks[4]
+    thumb_ip = landmarks[3]
+    thumb_mcp = landmarks[2]
     index_tip = landmarks[8]
     middle_tip = landmarks[12]
     ring_tip = landmarks[16]
     pinky_tip = landmarks[20]
-    wrist = landmarks[0]
 
-    if distance(thumb_tip, index_tip) < 0.05:
+    fingers = [
+        finger_is_open(landmarks, 8, 6),   # Index
+        finger_is_open(landmarks, 12, 10), # Middle
+        finger_is_open(landmarks, 16, 14), # Ring
+        finger_is_open(landmarks, 20, 18)  # Pinky
+    ]
+
+    # Thumb is open if tip is far from mcp and wrist
+    thumb_extended = (
+        np.linalg.norm(np.array([thumb_tip.x, thumb_tip.y]) - np.array([thumb_mcp.x, thumb_mcp.y])) > 0.07 and
+        np.linalg.norm(np.array([thumb_tip.x, thumb_tip.y]) - np.array([wrist.x, wrist.y])) > 0.12
+    )
+    # All other fingers closed
+    fingers_closed = not any(fingers)
+
+    # Thumbs up: thumb extended, others closed, thumb tip well above wrist
+    if thumb_extended and fingers_closed and (thumb_tip.y < wrist.y - 0.05):
+        return "thumbs_up"
+    # Thumbs down: thumb extended, others closed, thumb tip well below wrist
+    if thumb_extended and fingers_closed and (thumb_tip.y > wrist.y + 0.05):
+        return "thumbs_down"
+    # Pinch: thumb and index close, others closed
+    if np.linalg.norm(np.array([thumb_tip.x, thumb_tip.y]) - np.array([index_tip.x, index_tip.y])) < 0.04 and not any(fingers[1:]):
         return "pinch"
-    if all(landmarks[i].y < landmarks[i - 2].y for i in [8, 12, 16, 20]):
+    # Zoom: thumb and index far apart, others closed
+    if np.linalg.norm(np.array([thumb_tip.x, thumb_tip.y]) - np.array([index_tip.x, index_tip.y])) > 0.12 and not any(fingers[1:]):
+        return "zoom"
+    # Open palm: all fingers open, thumb not closed
+    thumb_closed = np.linalg.norm(np.array([thumb_tip.x, thumb_tip.y]) - np.array([wrist.x, wrist.y])) < 0.08
+    if all(fingers) and not thumb_closed:
         return "open_palm"
-    if all(distance(landmarks[i], wrist) < 0.1 for i in [8, 12, 16, 20]):
-        return "fist"
-    if (landmarks[8].y < landmarks[6].y and
-        all(landmarks[i].y > landmarks[i - 2].y for i in [12, 16, 20])):
-        return "pointing"
+    # Rotate left: index and middle open, hand tilted left
+    if fingers[0] and fingers[1] and not any(fingers[2:]) and (index_tip.x < wrist.x):
+        return "rotate_left"
+    # Rotate right: index and middle open, hand tilted right
+    if fingers[0] and fingers[1] and not any(fingers[2:]) and (index_tip.x > wrist.x):
+        return "rotate_right"
     return "unknown"
 
-# Start webcam
+STABLE_THRESHOLD = 7  # Increase for more strictness
+
+# Initialize webcam capture
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -50,7 +95,17 @@ while True:
             mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
             gesture = classify_gesture(hand_landmarks.landmark)
             cv2.putText(frame, gesture, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-            sio.emit('gesture', {'gesture': gesture})
+            if gesture == last_gesture:
+                gesture_count += 1
+            else:
+                gesture_count = 1
+                last_gesture = gesture
+            if connected and gesture_count >= STABLE_THRESHOLD and gesture != "unknown":
+                print("Emitting gesture:", gesture)
+                sio.emit('gesture', {'gesture': gesture})
+    else:
+        last_gesture = None
+        gesture_count = 0
 
     cv2.imshow("Hand Gesture", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -58,3 +113,4 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
